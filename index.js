250f188e31e260e2a33f3961a4b4c0d1b196eaeb
Tom Gregg <thomasgregg05@outlook.com>
2025-05-10 19:15:11 +0100

    add ejs and express js

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
+// Directives
 "use strict";
-
 // Imports
-import * as http from "http";
-// Constants
-const PORT = 8080;
-// Create server at 127.0.0.1:8080
-http
-  .createServer((req, res) => {
-    console.log(req.headers["user-agent"]);
-    switch (req.url) {
-      case "/comp50016":
-        res.end("SSP");
-        break;
-      default:
-        res.end("Hello World");
-        break;
-    }
-  })
-  .listen(PORT);
-// Log that the server is listening and on wha
+import express from "express";
+// Application Constants
+const app = express();
+const port = 8080;
+// Routes
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+// Start Server
+app.listen(port, () => {
+  console.log(`Express server listening at ${port}`);
+});
